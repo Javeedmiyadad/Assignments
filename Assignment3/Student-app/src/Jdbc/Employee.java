7f5569c8fd9f86ecefdb57769f927279e0525cc3
Javeed <miyadadjaveed@gmail.com>
2021-04-27 17:55:58 +0530

    simple app

---
+++ Assignment3/Student-app/src/Jdbc/Employee.java
@@ -0,0 +1,51 @@
+package Jdbc;
+
+//its like model
+public class Employee {
+    private int empId;
+    private String name;
+    private String email;
+
+    public Employee() {
+
+    }
+
+    public Employee(int empId, String name, String email) {
+        this.empId = empId;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empId=" + empId +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
